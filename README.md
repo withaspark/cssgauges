6372ce163bae04d4e090149708a5a6cfe2215858
Stephen Parker <web@withaspark.com>
2015-12-20 15:14:25 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# cssgauges
-CSS Gauges - easy CSS semi-circle gauges with Javascript optional.
+# CSS Gauges #
+
+Easy CSS semi-circle gauges with Javascript optional.
+
+For examples, see [example.html](example.html).
+
+
+## Usage ##
+
+### Gauges using JQuery ###
+
+For less markup and greater customization, use with with jQuery. Include some version of the jQuery library (I haven't tested compatibility with all versions yet). In the future, I think I'll remove the dependency on jQuery and rewrite with Javascript only, but until then...
+
+```
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<link href="gauges.css" rel="stylesheet" type="text/css">
+<script src="gauges.min.js"></script>
+```
+
+and create your markup with a class of ```gauge``` and specify at least the ```value``` attribute.
+
+```
+<div class="gauge" value=40></div>
+```
+
+This will create a default styled 40% filled gauge.
+
+### Gauges using only CSS ###
+
+Simply include the stylesheet
+
+```
+<link href="gauges.css" rel="stylesheet" type="text/css">
+```
+
+and create your markup with a class of ```gauge-<percentFilled>``` where ```<percentFilled>``` is the percentage of the gauge to fill. For example, a 40% filled gauge is created with the following markup:
+
+```
+<div class="gauge gauge-40">
+   <div class="outer">
+      <div class="inner"></div>
+      <div class="cover"></div>
+      <div class="value">40%</div>
+   </div>
+</div>
+```
+
+## Options ##
+
+Attribute|Type|Use
+---------|----|---
+```value```|```float```|Value of fille. Required.
+```animate```|```int|bool```|Whether the gauge should be animated and "dial up" to position. Accepts ```true```, ```false```, ```1```, ```0```. Optional. Default, ```0``` no animation.
+```bgcolor```|```string```|Color of the gauge background. Optional. Default, the ```background-color``` specified in css or inline for this ```gauge``` element, ```#fff```.
+```color```|```string```|Color of the gauge and value. Optional. Default, the ```color``` specified in css or inline for this ```gauge``` element, ```#8cc```.
+```max```|```float```|Value that results in gauge completely filled. Optional. Default, ```100```.
+```min```|```float```|Value that results in gauge completely unfilled. Optional. Default, ```0```.
+```notext```|```int|bool```|Whether the value should be hidden within the gauge. Accepts ```true```, ```false```, ```1```, ```0```. Optional. Default, ```0``` not hidden/value is displayed.
+```radius```|```int```|Radius of gauge in pixels. Optional. Default, double the ```height``` specified in css or inline for this ```gauge``` element, ```100px```.
+```thickness```|```int```|Thickness of gauge in pixels. Optional. Default, one-fifth the ```height``` specified in css or inline for this ```gauge``` element, ```10px```.
+```units```|```string```|Units of ```value```. Optional. Default, ```%```.
+
+## License ##
+
+Please feel free to use in your projects (personal and commercial) under the terms of the [MIT License](LICENSE). Pull requests greatly appreciated!
---
+++ gauges.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+	$(".gauge").each(function() {
+		// If using shorthand, fill in
+		if ($(this).children().length == 0) {
+			var fVal = $(this).attr("value");
+			var sUnits = $(this).attr("units");
+				// Assume units if not provided
+				if (typeof sUnits == 'undefined') sUnits = "%";
+			var fMax = $(this).attr("max");
+				// Assume max if not provided
+				if (typeof fMax == 'undefined') fMax = 100.0;
+			var fMin = $(this).attr("min");
+				// Assume min if not provided
+				if (typeof fMin == 'undefined') fMin = 0.0;
+			var sColor = $(this).attr("color");
+				// Assume color if not provided
+				if (typeof sColor == 'undefined') sColor = $(this).css("color");
+			var sBgColor = $(this).attr("bgcolor");
+				// Assume background color if not provided
+				if (typeof sBgColor == 'undefined') sBgColor = $(this).css("background-color");
+			var iRadius = $(this).attr("radius");
+				// Assume radius if not provided
+				if (typeof iRadius == 'undefined') iRadius = 2*$(this).height();
+			var iThickness = $(this).attr("thickness");
+				// Assume thickness if not provided
+				if (typeof iThickness == 'undefined') iThickness = $(this).height()/5;
+			var sAnimate = $(this).attr("animate");
+				// Assume animate if not provided
+				if (typeof sAnimate == 'undefined') sAnimate = "0";
+			var sNoText = $(this).attr("notext");
+				// Assume notext if not provided
+				if (typeof sNoText == 'undefined') sNoText = "0";
+
+			// Calculate percentage before assuming units
+			var fPercentage = (fVal-fMin)/(fMax-fMin);
+
+			// Construct gauge
+			$(this).append('<div class="outer"><div class="inner"></div><div class="cover"></div><div class="value">'+fVal+sUnits+'</div></div>');
+
+			// If specified custom radius, style radius, thickness, and size
+			$(this).css({
+				"height": (iRadius/2)+"px",
+				"width": (iRadius)+"px"
+			});
+			$(this).children(".outer").css({
+				"width": (iRadius)+"px",
+				"height": (iRadius)+"px"
+			});
+			$(this).children(".outer").children(".inner").css({
+				"width": (iRadius-(2*iThickness))+"px",
+				"height": (iRadius-(2*iThickness))+"px",
+				"top": (iThickness)+"px",
+				"left": (iThickness)+"px"
+			});
+			$(this).children(".outer").children(".value").css({
+				"font-size": ((iRadius-(2*iThickness))/5)+"px"
+			});
+			
+			// With animate
+			if (stringToBool(sAnimate)) {
+				$(this).children(".outer").children(".cover").animate(
+					{fakeNumericProp: (180*fPercentage)},
+					{
+						step: function(current, effect) {
+							$(this).css('transform','rotate('+current+'deg)'); 
+						},
+						duration: 2000
+					}
+				);
+			}
+			// Without animate
+			else {
+				$(this).children(".outer").children(".cover").css("transform", "rotate("+(180*fPercentage)+"deg)");
+			}
+
+			// Style
+			$(this).css({
+				"background-color": sBgColor,
+				"color": sColor
+			});
+			$(this).children(".outer").css({"background-color": sColor});
+			$(this).children(".outer").children(".inner").css({"background-color": sBgColor});
+			$(this).children(".outer").children(".cover").css({"background-color": sBgColor});
+
+			// If notext, hide value display
+			if (stringToBool(sNoText))
+				$(this).children(".outer").children(".value").css({"display": "none"});
+		}
+	});
+});
+
+/**
+ * Converts string values to a boolean
+ */
+function stringToBool(value) {
+	return (value == "1" || value.toLowerCase() == "true") ? true : false;
+}
